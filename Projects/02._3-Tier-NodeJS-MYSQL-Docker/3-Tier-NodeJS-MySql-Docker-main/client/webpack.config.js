8e4ca3c20f7b6aa2634fc15356c4c61c53cb8dc0
Your Name <you@example.com>
2026-03-16 20:24:11 +1100

    updated code

---
+++ Projects/02._3-Tier-NodeJS-MYSQL-Docker/3-Tier-NodeJS-MySql-Docker-main/client/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), // Changed to 'dist' to match Dockerfile
+  },
+  optimization: {
+    minimize: false, // Disable minification to avoid terser-webpack-plugin error
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.css', '.png'],
+  },
+};
+
